737916174ab259c818624c68ab1e138955fe0c58
cemiluzunhasan <cmluzunhasan@gmail.com>
2020-03-19 11:37:45 +0300

    Comments

--- components/Pages/About/Members.js
+++ components/Pages/About/Members.js
@@ -1,6 +1,11 @@
 import { MEMBERS } from '../../../data/about';
 
 const Members = () => {
+  
+  const goTo = (link) => {
+    window.location.replace(link);
+  };
+
   return (
     <div className="Members">
       <h1 className="text-3xl text-center text-bold">Üyelerimiz</h1>
@@ -10,6 +15,11 @@ const Members = () => {
             <img src={member.photo} className="MemberImage" />
             <h1 className="MemberName text-center text-bold">{member.name}</h1>
             <h2 className="MemberRole text-center">{member.role}</h2>
+            <div className="MemberAccounts">
+              { member.accounts.map(account => {
+                <img onClick={() => goTo(account.href)} src={`/static/icons/social/${account.id}.svg`} className="MemberAccount" />
+              })}
+            </div>
           </div>
         ))}
       </div>
--- data/about.js
+++ data/about.js
@@ -1,26 +1,99 @@
+// - President is on the top, members are below. Structure should be president and members
 export const MEMBERS = [
   {
     id: 1,
     name: 'Ezgi Deniz Yıldırım',
     role: 'Başkan',
-    photo: '/static/img/about/ezgi.png'
+    photo: '/static/img/about/ezgi.png',
+    accounts: [
+      {
+        id: 'facebook',
+        href: 'www.facebook.com'
+      },
+      {
+        id: 'instagram',
+        href: 'https://www.instagram.com'
+      },
+      {
+        id: 'twitter',
+        href: 'https://www.twitter.com'
+      },
+      {
+        id: 'linkedin',
+        href: 'https://www.linkedin.com'
+      },
+    ]
   },
   {
     id: 2,
     name: 'Ezgi Deniz Yıldırım',
     role: 'Başkan',
-    photo: '/static/img/about/ezgi.png'
+    photo: '/static/img/about/ezgi.png',
+    accounts: [
+      {
+        id: 'facebook',
+        href: 'www.facebook.com'
+      },
+      {
+        id: 'instagram',
+        href: 'https://www.instagram.com'
+      },
+      {
+        id: 'twitter',
+        href: 'https://www.twitter.com'
+      },
+      {
+        id: 'linkedin',
+        href: 'https://www.linkedin.com'
+      },
+    ]
   },
   {
     id: 3,
     name: 'Ezgi Deniz Yıldırım',
     role: 'Başkan',
-    photo: '/static/img/about/ezgi.png'
+    photo: '/static/img/about/ezgi.png',
+    accounts: [
+      {
+        id: 'facebook',
+        href: 'www.facebook.com'
+      },
+      {
+        id: 'instagram',
+        href: 'https://www.instagram.com'
+      },
+      {
+        id: 'twitter',
+        href: 'https://www.twitter.com'
+      },
+      {
+        id: 'linkedin',
+        href: 'https://www.linkedin.com'
+      },
+    ]
   },
   {
     id: 4,
     name: 'Ezgi Deniz Yıldırım',
     role: 'Başkan',
-    photo: '/static/img/about/ezgi.png'
+    photo: '/static/img/about/ezgi.png',
+    accounts: [
+      {
+        id: 'facebook',
+        href: 'www.facebook.com'
+      },
+      {
+        id: 'instagram',
+        href: 'https://www.instagram.com'
+      },
+      {
+        id: 'twitter',
+        href: 'https://www.twitter.com'
+      },
+      {
+        id: 'linkedin',
+        href: 'https://www.linkedin.com'
+      },
+    ]
   },
 ];
